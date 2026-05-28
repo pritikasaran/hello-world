3c6157e799aa69caa278be538e26fefb07183152
pritikasaran <pritikasaran14@gmail.com>
2026-05-28 07:47:45 +0530

    Update prop.java

--- prop.java
+++ prop.java
@@ -1,91 +1,56 @@
-package de.deltalloyd.partnerdb.webservices.impl;
+package de.deltalloyd.partnerdb.rest.restWrapper;
 
 import java.util.List;
 
 import jakarta.ejb.EJB;
-import jakarta.ejb.Stateless;
-import jakarta.jws.WebMethod;
-import jakarta.jws.WebParam;
-import jakarta.jws.WebService;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import de.deltalloyd.foundation.exception.AbstractCheckedSystemException;
-import de.deltalloyd.partnerdb.core.service.AxaMaklerService;
-import de.deltalloyd.partnerdb.orb.impl.OrbServiceEJB;
-import de.deltalloyd.partnerdb.security.profiles.PdbBerechtigungen;
-import de.deltalloyd.partnerdb.security.services.SecurityTokenSevice;
-import de.deltalloyd.partnerdb.webservices.exception.PDBException;
 import de.deltalloyd.partnerdb.webservices.exception.PartnerWebServiceException;
-import de.deltalloyd.partnerdb.webservices.utils.PDBReleaseInfo;
+import de.deltalloyd.partnerdb.webservices.impl.AxaMaklerWebServiceDelegate;
 
-@Stateless
-@WebService(name = "AxaMaklerWebService", serviceName = "AxaMaklerWebService")
-public class AxaMaklerWebService {
+@Path("/api/axamakler")
+@ApplicationScoped
+@Consumes(MediaType.APPLICATION_XML)
+@Produces(MediaType.APPLICATION_XML)
+public class AxaMaklerRestWrapper {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(AxaMaklerWebService.class);
+    @EJB
+    private AxaMaklerWebServiceDelegate axaMaklerService;
 
-	@PersistenceContext(unitName = "PDBPOOL")
-	protected EntityManager emPDBPool;
+    @GET
+    @Path("/findByExterneReferenz")
+    public List<Long> findAxaPartnerByExterneReferenz(
+            @QueryParam("authToken") String authToken,
+            @QueryParam("externeReferenz") String externeReferenz) throws PartnerWebServiceException {
+        return axaMaklerService.findAxaPartnerByExterneReferenz(authToken, externeReferenz);
+    }
 
-	@EJB
-	private OrbServiceEJB orbServiceEJB;
+    @GET
+    @Path("/checkADMAOrb")
+    public Boolean checkIfADMAOrbFuerAxaMaklerExists(
+            @QueryParam("authToken") String authToken,
+            @QueryParam("admaSchluessel") String admaSchluessel) throws PartnerWebServiceException {
+        return axaMaklerService.checkIfADMAOrbFuerAxaMaklerExists(authToken, admaSchluessel);
+    }
 
-	@EJB
-	private AxaMaklerService axaMaklerService;
+    @POST
+    @Path("/erzeugeADMAOrb")
+    public Boolean erzeugeADMAOrbFuerAxaMakler(
+            @QueryParam("authToken") String authToken,
+            @QueryParam("partnerNr") Long partnerNr) throws PartnerWebServiceException {
+        return axaMaklerService.erzeugeADMAOrbFuerAxaMakler(authToken, partnerNr);
+    }
 
-	@EJB
-	private SecurityTokenSevice sts;
-
-	@WebMethod
-	public List<Long> findAxaPartnerByExterneReferenz(@WebParam(name = "authToken") final String authToken,
-			@WebParam(name = "externeReferenz") String externeReferenz) throws PartnerWebServiceException {
-		try {
-			sts.isAllowed(authToken, PdbBerechtigungen.PDB_LESEN);
-		} catch (AbstractCheckedSystemException e) {
-			throw new PartnerWebServiceException(
-					new PDBException(PDBException.AbstractCheckedSystemException, e.getMessage(), e.getUuid()));
-		}
-		return axaMaklerService.findAxaPartnerByExterneReferenz(externeReferenz);
-	}
-
-	@WebMethod
-	public Boolean checkIfADMAOrbFuerAxaMaklerExists(@WebParam(name = "authToken") final String authToken,
-			@WebParam(name = "admaSchluessel") String admaSchluessel) throws PartnerWebServiceException {
-		try {
-			sts.isAllowed(authToken, PdbBerechtigungen.PDB_LESEN);
-		} catch (AbstractCheckedSystemException e) {
-			throw new PartnerWebServiceException(
-					new PDBException(PDBException.AbstractCheckedSystemException, e.getMessage(), e.getUuid()));
-		}
-		return axaMaklerService.checkIfADMAOrbFuerAxaMaklerExists(admaSchluessel);
-	}
-
-	@WebMethod
-	public Boolean erzeugeADMAOrbFuerAxaMakler(@WebParam(name = "authToken") final String authToken,
-			@WebParam(name = "partnerNr") Long partnerNr) throws PartnerWebServiceException {
-		boolean result = false;
-
-		try {
-			sts.isAllowed(authToken, PdbBerechtigungen.PDB_LESEN);
-
-			LOGGER.debug("erzeugeADMAOrbFuerAxaMakler with partnerNr: {}", partnerNr);
-			result = axaMaklerService.erzeugeADMAOrbFuerAxaMakler(partnerNr);
-
-		} catch (AbstractCheckedSystemException e) {
-			throw new PartnerWebServiceException(
-					new PDBException(PDBException.AbstractCheckedSystemException, e.getMessage(), e.getUuid()));
-		}
-
-		return result;
-	}
-
-	@WebMethod
-	public String getReleaseInfo() {
-		return PDBReleaseInfo.getReleaseInfo();
-	}
-
+    @GET
+    @Path("/releaseInfo")
+    public String getReleaseInfo() {
+        return axaMaklerService.getReleaseInfo();
+    }
 }
