ad13a8af487fe879f9b9ae46dd47dcb9297a20fe
pritikasaran <pritikasaran14@gmail.com>
2026-07-13 18:24:57 +0530

    Update prop.java

--- prop.java
+++ prop.java
@@ -1,5 +1,13 @@
-  writeLine((urlParamsStarted ? "&" : "?") 
-            + name + "=" + URLEncoder.encode(value, StandardCharsets.UTF_8), writer);
-        urlParamsStarted = true;
+window.onload = function() {
+    var widthPercent = 0.35;
+    var heightPercent = 0.85;
 
+    var width = Math.round(window.screen.availWidth * widthPercent);
+    var height = Math.round(window.screen.availHeight * heightPercent);
+    var left = window.screen.availWidth - width;
+    var top = Math.round((window.screen.availHeight - height) / 2);
+
+    window.resizeTo(width, height);
+    window.moveTo(left, top);
+};
 
