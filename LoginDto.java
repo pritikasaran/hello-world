a1352ba99d06948f3312fd4d60564744db81ce7b
pritikasaran <pritikasaran14@gmail.com>
2026-07-29 23:21:59 +0530

    Update LoginDto.java

--- LoginDto.java
+++ LoginDto.java
@@ -1,21 +1,10 @@
-package de.deltalloyd.partnerdb.rest.DTO.sts;
-
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "login")
-@XmlAccessorType(XmlAccessType.FIELD)
-public class LoginDto {
-
-    private String userId;
-    private String pw;
-
-    public LoginDto() {}
-
-    public String getUserId() { return userId; }
-    public void setUserId(String userId) { this.userId = userId; }
-
-    public String getPw() { return pw; }
-    public void setPw(String pw) { this.pw = pw; }
-}
+document.addEventListener('keydown', function(e) {
+    var scrollBody = document.querySelector('.ui-datatable-scrollable-body');
+    if (!scrollBody) return;
+    
+    if (e.key === 'ArrowDown') {
+        scrollBody.scrollTop += 30;
+    } else if (e.key === 'ArrowUp') {
+        scrollBody.scrollTop -= 30;
+    }
+});
